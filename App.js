f9b7ece06f9448f6e9dfdb7fecf5f7f2773c60cf
okamuuu <okamuuu@gmail.com>
2019-01-29 10:55:38 +0900

    add: react-navigation

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import React from 'react';
+import { Button, View } from 'react-native';
 import { Container, Header, Content, List, ListItem, Text } from 'native-base';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; 
 
-export default class App extends React.Component {
-
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-    });
-  }
-
+class HomeScreen extends React.Component {
   render() {
     return (
       <Container>
@@ -17,7 +11,7 @@ export default class App extends React.Component {
         <Content>
           <List>
             <ListItem>
-              <Text>Firebase Authentication</Text>
+              <Text onPress={() => this.props.navigation.navigate('Details')}>Firebase Authentication</Text>
             </ListItem>
             <ListItem>
               <Text>xxxx</Text>
@@ -29,5 +23,45 @@ export default class App extends React.Component {
         </Content>
       </Container>
     );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }
+
+  render() {
+    return <AppContainer />;
   }
 }
